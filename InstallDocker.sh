507f1f3d6e4c1a65b73f99980a6a8f1de9e6fa59
Arthobelly_sous linux <arthobellykiasilua@gmail.com>
2022-09-10 14:39:41 +0100

    version 1 script Docker

---
+++ InstallDocker.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "script ecrit par" ${USER}
+echo "---------------------------------------------------------"
+echo "ce script aide à l'installation de Docker sur ubuntu20"
+echo "---------------------------------------------------------"
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+echo "---------------------------------------------------------"
+echo "ajout de la clé GPG au systeme"
+echo "---------------------------------------------------------"
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo "---------------------------------------------------------"
+echo "ajout du referentiel au source APT"
+echo "---------------------------------------------------------"
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+echo "---------------------------------------------------------"
+echo "update des paquets"
+echo "---------------------------------------------------------"
+
+sudo apt update
+
+echo "---------------------------------------------------------"
+echo "verification de l'installation à partir du depot officiel docker"
+echo "---------------------------------------------------------"
+
+sudo apt-cache policy docker-ce
+
+echo "---------------------------------------------------------"
+echo "installation decker"
+echo "---------------------------------------------------------"
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+
+echo "---------------------------------------------------------"
+echo "version docker"
+echo "---------------------------------------------------------"
+
+docker --version
+
+# pour eviter de taper à chaque fois sudo pour docker
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
+docker run hello-world
+docker search ubuntu
+
+echo "---------------------------------------------------------"
+echo "telechargement image ubuntu"
+echo "---------------------------------------------------------"
+docker pull ubuntu
+
+docker images
