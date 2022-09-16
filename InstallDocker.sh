0eca4074acb475a1e7a22ad858f434797ca26b04
arthobelly <kiasiluarthobelly@gmail.com>
2022-09-16 11:58:10 +0100

    arrangement InstallDocker

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -58,3 +58,6 @@ echo "---------------------------------------------------------"
 docker pull ubuntu
 
 docker images
+sudo apt-get update
+sudo apt-get install docker-compose-plugin
+docker compose version
